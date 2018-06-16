6043ed115d26dbbd21196408dcd9fd27797b9055
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2018-06-16 03:46:55 +0300

    Refreshed dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -3,4 +3,4 @@
  * @author  Nazar Mokrynskyi <nazar@mokrynskyi.com>
  * @license 0BSD
  */
-require('build-gc')('src/index.js', 'src/index.min.js', 'src/externs.js');
+require('build-gc').default('src/index.js', 'src/index.min.js', 'src/externs.js');
